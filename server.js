2d5ff66a5bd609674d56534724519dfaf82d97d7
debabiswa79315 <31183662+debabiswa79315@users.noreply.github.com>
2017-08-26 22:12:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/article-one',function (req, res){
     res.send('article one requested will be served here');
 });
 app.get('/article-two',function (req, res){
-    res.send('article one requested will be served here');
+   res.sendFile(path.join(__dirname, 'ui', 'article-one.html')); 
 });
 app.get('/article-three',function (req, res){
     res.send('article one requested will be served here');
